9f36ee84110ec5c871c8b3cc325c0332b7931c39
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-05-04 00:59:19 +0600

    added functionality in the log in enterface

--- SCRIPT JS/login.js
+++ SCRIPT JS/login.js
@@ -2,5 +2,30 @@
 //console.log('connection');
 
 document.getElementById('login-btn').addEventListener('click', function(){
-    console.log('log in');
+
+    // STEP 1-- INPUT MOBILE NUMBER
+    const numberInput = document.getElementById('input-number');
+    const newNumber = numberInput.value;
+    console.log(newNumber);
+
+    // STEP 2 -- GET THE PIN
+    const pinInput = document.getElementById('input-pin');
+    const newPin = pinInput.value;
+    console.log(newPin);
+
+    // STEP 3 -- MATCH PIN AND MOBILE NUMBER
+    if(newNumber == '01818808523' && newPin == '1234')
+    {
+        // ALERT SUCCESSFUL
+        alert('log in successful');
+
+
+        window.location.assign('../homePage.html');
+    }
+    else
+    {
+        // IF LOG IN FAILED
+        alert('log in failed');
+        return;
+    }
 })
