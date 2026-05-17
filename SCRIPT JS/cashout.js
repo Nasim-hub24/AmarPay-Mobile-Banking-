606ddcc4711edbfe5fe1909e24df8c52c41a0b07
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-05-18 03:09:24 +0600

    functionality of cash out

--- SCRIPT JS/cashout.js
+++ SCRIPT JS/cashout.js
@@ -1,1 +1,48 @@
+//console.log('connection successful')
 
+document.getElementById('cashOut-btn').addEventListener('click', function(){
+
+    //1-- get the agent number
+    const cashOutNumberInput = document.getElementById('cashOut-number');
+    const phoneNumber = cashOutNumberInput.value;
+    console.log(phoneNumber);
+    if(phoneNumber.length != 11){
+        alert('wrong phone number');
+        return;
+    }
+
+    //2-- get the current amount
+    const cashOutAmount = document.getElementById('cashout-amount');
+    const amount = cashOutAmount.value;
+    console.log(amount);
+
+    //3-- get the current balance
+    const balanceElement = document.getElementById('balance');
+    const currentBalance = balanceElement.innerText;
+    console.log(currentBalance);
+
+    //4-- calculate new balance
+    const newBalance = Number(currentBalance)- Number(amount);
+    if(newBalance < 0)
+    {
+        alert('invelid amount');
+        return;
+    }
+    
+
+    //5-- Verify Pin
+    const cashOutPinInput = document.getElementById('cashOut-pin');
+    const pin = cashOutPinInput.value;
+
+    if(pin === '1234')
+    {
+        alert('correct pin');
+        console.log('new balance', newBalance);
+        balanceElement.innerText = newBalance;
+    }
+    else
+    {
+        alert('invalid pin');
+        return;
+    }
+})
